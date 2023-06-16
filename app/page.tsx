148ae2b2a7b6c041f65bfa24ba98a4858b059cee
Kevin Tame <kevintame@gmail.com>
2023-06-16 01:28:33 -0600

    update favicon

--- app/page.tsx
+++ app/page.tsx
@@ -3,29 +3,32 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="relative grid mx-auto max-w-2xl p-10 sm:p-24">
-      <div className=''>
+      <div className='mx-auto'>
         <Image
           className=""
           src="/rats.svg"
           alt="Rats Logo Dark"
-          width={180}
-          height={180}
+          width={80}
+          height={80}
           priority
           />  
-            </div>    
+            </div> 
+            <div className='text-center'>
           <h1 className="mt-10 text-3xl font-bold tracking-tight text-black dark:text-white sm:text-4xl">
             Rats is no longer available.
           </h1>
           <p className="mt-6 text-lg sm:text-2xl text-black dark:text-white">
             Thanks for all your support!
           </p>
+            </div>   
           <div className='w-full justify-left mt-20'>
           <p className="text-xl font-bold tracking-tight text-black dark:text-white sm:text-2xl">
             Why?
           </p>
           <p className="mt-6 text-black dark:text-white">
-            We built some awesome stuff and many users loved what we did but unfortunatly 
-            we weren’t able to achieve product-market fit and get the the user base we needed to make it a viable business. 😢 
+            We built some awesome stuff and were well-received by a substantial number of users, who appreciated our efforts. 
+            Unfortunatly, we fell short of finding a product-market fit and getting a big enough user base necessary to 
+            ensure the business’s viability. 😢
           </p>
         </div>   
     </main>
